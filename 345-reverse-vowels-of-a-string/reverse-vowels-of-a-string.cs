ad5b24db81039fa67eccf4cb161fc4ed27021db7
Burakcems <burakcems16@gmail.com>
2025-11-22 23:39:04 +0300

    Time: 2 ms (99.79%) | Memory: 45.1 MB (88.81%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public string ReverseVowels(string s) {
+        char[] chars = s.ToCharArray();
+
+        int left = 0;
+        int right = s.Length-1;
+
+        string vowels ="aeiouAEIOU";
+
+        while ( left<right)
+        {
+            while (left < right && !vowels.Contains(chars[left]))
+            {
+                left++;
+            }
+            while (left < right && !vowels.Contains(chars[right])) {
+                right--;
+            }
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+        return new string(chars);
+    }
+}
