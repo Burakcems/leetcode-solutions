9df48e997adc8cfb492b639ca8320e8c8ec36fba
Burakcems <burakcems16@gmail.com>
2025-11-26 11:21:17 +0300

    Time: 1 ms (95.67%) | Memory: 58.5 MB (63.36%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+        
+        int zero = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                int temp = nums[zero];
+                nums[zero] = nums[i];
+                nums[i] = temp;
+
+                zero++;
+            }
+            
+        }
+    }
+}
